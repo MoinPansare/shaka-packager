5218df8ecb0b0f503a9d601b0b12ce8b01af7002
Moin <moin.pansare@gmail.com>
2020-03-04 21:28:53 -0700

    log

--- packager/file/udp_file.cc
+++ packager/file/udp_file.cc
@@ -204,8 +204,8 @@ bool UdpFile::Open() {
     if (options->is_source_specific_multicast()) {
       struct ip_mreq_source source_multicast_group;
 
-      DVLOG(1) << "Interface : " << options->interface_address();
-      DVLOG(1) << "Source : " << options->source_address();
+      LOG(INFO) << "Interface : " << options->interface_address();
+      LOG(INFO) << "Source : " << options->source_address();
 
 
       source_multicast_group.imr_multiaddr = local_in_addr;
